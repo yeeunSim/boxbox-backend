0df9f41b137b04a9691a4191d4d4cf9901f17a45
yeeunSim <wjdwlr03@gmail.com>
2025-11-07 22:24:46 +0900

    Test: jacoco를 이용해 코드 커버리지 측정 환경 세팅

--- src/test/java/com/showrun/boxbox/BoxboxApplicationTests.java
+++ src/test/java/com/showrun/boxbox/BoxboxApplicationTests.java
@@ -2,8 +2,14 @@ package com.showrun.boxbox;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"ncp.papago.client-id=dummy-client-id",
+		"ncp.papago.client-secret=dummy-client-secret"
+})
 class BoxboxApplicationTests {
 
 	@Test
